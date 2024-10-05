58c2594eadae9b1b72a2c1a5ed9c4e424a81fdf7
Mykhailo Budiakov <mishabudjakov@gmail.com>
2024-10-05 11:56:45 +0300

    - Add description to achievements

--- src/achievements.js
+++ src/achievements.js
@@ -6,8 +6,7 @@ class AchievementForm extends FormApplication {
     }
 
     getData() {
-        return foundry.utils.mergeObject(super.getData(), {
-        });
+        return foundry.utils.mergeObject(super.getData(), {});
     }
 
     static get defaultOptions() {
@@ -18,47 +17,56 @@ class AchievementForm extends FormApplication {
             template: "modules/pf2e-party-sheet-helper/templates/achievements.hbs",
             width: 500,
             height: "auto",
-            closeOnSubmit: true,
+            closeOnSubmit: false,
             submitOnChange: false,
             resizable: true,
         });
     }
 
     async _updateObject(_event, updateData) {
-        if (!updateData?.name || !updateData["texture.src"]) { return }
+        if (!updateData?.name || !updateData["texture.src"]) {
+            ui.notifications.warn("Name and image fields are missing");
+            return
+        }
 
         let data = this.actor.getFlag(moduleName, "achievements") ?? [];
         data.push({
             text: updateData.name,
+            description: updateData.description,
             img: updateData["texture.src"],
         });
 
         this.actor.setFlag(moduleName, "achievements", data)
+        this.close()
     }
 }
 
-Hooks.on('getPartySheetPF2eHeaderButtons', function(partySheet, buttons) {
-    if (!game.user.isGM) {return;}
+Hooks.on('getPartySheetPF2eHeaderButtons', function (partySheet, buttons) {
+    if (!game.user.isGM) {
+        return;
+    }
     buttons.unshift({
-        label: "Achievements",
+        label: "",
         icon: "fa fa-trophy",
         class: `${moduleName}-achievements`,
         onclick: () => {
-            (new AchievementForm({actor:partySheet.actor})).render(true);
+            (new AchievementForm({actor: partySheet.actor})).render(true);
         }
     });
 });
 
-Hooks.on('renderPartySheetPF2e', function(partySheet, html) {
-    if (!game.settings.get(moduleName, "showAchievements")) {return}
+Hooks.on('renderPartySheetPF2e', function (partySheet, html) {
+    if (!game.settings.get(moduleName, "showAchievements")) {
+        return
+    }
 
     let data = partySheet.actor.getFlag(moduleName, "achievements") ?? [];
-    let content = data.sort((a,b) => a.text.localeCompare(b.text)).map((a,idx)=>`<div class="portrait" data-tooltip="${a.text}" data-index=${idx}>${game.user.isGM?'<i class="fas fa-times close"></i>' : ''}<img src="${a.img}"><label>${a.text}</label></div>`).join("")
+    let content = data.sort((a, b) => a.text.localeCompare(b.text)).map((a, idx) => `<div class="portrait" data-tooltip="${a.description}" data-index=${idx}>${game.user.isGM ? '<i class="fas fa-times close"></i>' : ''}<img src="${a.img}"><label>${a.text}</label></div>`).join("")
 
     html.find('.sub-nav').append('<a data-tab="achievements" class="">Achievements</a>');
     html.find('.container').append(`<div class="tab" data-tab="achievements" data-region="achievements"><div class="achievements content">${content}</div></div>`);
 
-    html.find('.achievements .close').on("click", async function(event) {
+    html.find('.achievements .close').on("click", async function (event) {
         const target = $(event.currentTarget);
         const dataIdx = target.parent().data().index;
 
